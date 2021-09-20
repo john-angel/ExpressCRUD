ce7d3e5329c668df5df8747c7c61d69cd6d61736
John Angel <john.angel@endava.com>
2021-09-20 18:46:29 -0500

    style: add documentation

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,29 +1,5 @@
 const Users = require('./../models/users')
 
-// Use class or functions
-function helloWorld(req, res) {
-    const userDB = new Users({
-        name: 'EndavaUser',
-        age: 18,
-        password: '123456'
-    })
-    try {
-        userDB.save( (err, user) => {
-            if(err) throw Error(err);
-            console.log('user created');
-            return res.status(200).json({
-                status: 200,
-                response: user
-            })
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: 400,
-            response: error
-        })
-    }
-}
-
 async function getUsers(req, res){
     const users = await Users.find({})
     console.log('Users:', users);
@@ -31,6 +7,10 @@ async function getUsers(req, res){
 }
 
 async function getUser(req, res){
+
+    /*
+    Get user only by id
+    */
  
     try {
         const user = await Users.findById(req.params.id).exec();
@@ -109,6 +89,9 @@ async function editUser(req, res){
 async function deleteUser(req, res) {
 
   try {
+      /*
+      Delete by any attribute
+      */
 
       await Users.deleteOne(req.body);
 
